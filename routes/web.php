3396ad9a43dec441e1212048b1d9892293b88c4b
Komang Gede Narariya Suputra <91647402+16Komang@users.noreply.github.com>
2022-12-28 12:00:57 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -130,6 +130,7 @@ Route::get('/mig', function () {
     // Call and Artisan command from within your application.
     Artisan::call('migrate:fresh');
     Artisan::call('db:seed');
+    shell_exec("npm run dev");
 });
 
 Route::get('/cc', function () {
